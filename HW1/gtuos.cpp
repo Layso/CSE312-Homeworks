a3a6339a75a4d113077023913b14b37a1c03b252
Can <laysogame@hotmail.com>
2018-03-25 15:32:24 +0300

    Operation templates created

--- HW1/gtuos.cpp
+++ HW1/gtuos.cpp
@@ -2,8 +2,66 @@
 #include "8080emuCPP.h"
 #include "gtuos.h"
 
+
+
+
+
 uint64_t GTUOS::handleCall(const CPU8080 & cpu){
-	std::cout << "Derdin ne evladım senin?\n";
-	std::cout << cpu.lastOpcode << std::endl;
-	return 0;
+	std::cout << "SysCall recieved\n";
+	
+	
+	switch(cpu.state->a) {
+		case GTUOS::PRINT_B :	OperationPrintB(cpu); break;
+		case GTUOS::PRINT_MEM :	OperationPrintMem(cpu); break;
+		case GTUOS::READ_B :	OperationReadB(cpu); break;
+		case GTUOS::READ_MEM :	OperationReadMem(cpu); break;
+		case GTUOS::PRINT_STR :	OperationPrintStr(cpu); break;
+		case GTUOS::READ_STR :	OperationReadStr(cpu); break;
+		case GTUOS::GET_RND :	OperationGetRnd(cpu); break;
+		default: std::cout << "Unimplemented systam call\n"; exit(EXIT_FAILURE);
+	}
 }
+
+
+
+void GTUOS::OperationPrintB(const CPU8080 &cpu) {
+	
+}
+
+
+
+void GTUOS::OperationPrintMem(const CPU8080 &cpu) {
+	
+}
+
+
+
+void GTUOS::OperationReadB(const CPU8080 &cpu) {
+	
+}
+
+
+
+void GTUOS::OperationReadMem(const CPU8080 &cpu) {
+	
+}
+
+
+
+void GTUOS::OperationPrintStr(const CPU8080 &cpu) {
+	
+}
+
+
+
+void GTUOS::OperationReadStr(const CPU8080 &cpu) {
+	
+}
+
+
+
+void GTUOS::OperationGetRnd(const CPU8080 &cpu) {
+	
+}
+
+
--- HW1/gtuos.h
+++ HW1/gtuos.h
@@ -4,9 +4,31 @@
 #include "8080emuCPP.h"
 
 
+
 class GTUOS{
 	public:
 		uint64_t handleCall(const CPU8080 & cpu);
+		
+		/* Const definitions for opcodes to compare */
+		enum Constants {
+            PRINT_B = 1,
+            PRINT_MEM = 2,
+			READ_B = 3,
+			READ_MEM = 4,
+			PRINT_STR = 5,
+			READ_STR = 6,
+			GET_RND = 7
+        };
+				
+	private:
+		/* Functions to operate according to system call */
+		void OperationPrintB(const CPU8080 &cpu);
+		void OperationPrintMem(const CPU8080 &cpu);
+		void OperationReadB(const CPU8080 &cpu);
+		void OperationReadMem(const CPU8080 &cpu);
+		void OperationPrintStr(const CPU8080 &cpu);
+		void OperationReadStr(const CPU8080 &cpu);
+		void OperationGetRnd(const CPU8080 &cpu);
 };
 
 #endif
