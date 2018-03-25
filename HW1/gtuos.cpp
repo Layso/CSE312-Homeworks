598267a86dfa351e6a21c8e2f261b3dfc5154559
Can <laysogame@hotmail.com>
2018-03-25 17:05:03 +0300

    Implementation started for basic system call operations

--- HW1/gtuos.cpp
+++ HW1/gtuos.cpp
@@ -4,13 +4,16 @@
 
 
 
-
+/* Constant value(s) */
+const int BYTE_BIT_COUNT = 8;
 
 uint64_t GTUOS::handleCall(const CPU8080 & cpu){
-	std::cout << "SysCall recieved\n";
+	int op = 7;
+	std::cout << "SysCall recieved: " << (int)cpu.state->a << std::endl;
 	
 	
-	switch(cpu.state->a) {
+	
+	switch(op) {
 		case GTUOS::PRINT_B :	OperationPrintB(cpu); break;
 		case GTUOS::PRINT_MEM :	OperationPrintMem(cpu); break;
 		case GTUOS::READ_B :	OperationReadB(cpu); break;
@@ -25,43 +28,80 @@ uint64_t GTUOS::handleCall(const CPU8080 & cpu){
 
 
 void GTUOS::OperationPrintB(const CPU8080 &cpu) {
+	int value;
 	
+	
+	/* Reading value from register B */
+	value = (int)cpu.state->b;
+	
+	/* Printing value to standart output */
+	std::cout << value << std::endl;
 }
 
 
 
 void GTUOS::OperationPrintMem(const CPU8080 &cpu) {
+	uint16_t address;
+	int value;
 	
+	
+	/* Reading value from memory */
+	address = cpu.state->b;
+	address = (address << BYTE_BIT_COUNT) | cpu.state->c;
+	value = (int)cpu.memory->at(address);
+	
+	/* Printing value to standart output */
+	std::cout << value << std::endl;
 }
 
 
 
 void GTUOS::OperationReadB(const CPU8080 &cpu) {
-	
+	/* 
+	 * TODO:
+	 * Read an integer from stdin
+	 * Write integer to register B
+	 */
 }
 
 
 
 void GTUOS::OperationReadMem(const CPU8080 &cpu) {
-	
+	/*
+	 * TODO:
+	 * Read an integer from stdin
+	 * Write integer to mem[CB]
+	 */
 }
 
 
 
 void GTUOS::OperationPrintStr(const CPU8080 &cpu) {
-	
+	/*
+	 * TODO:
+	 * Get string from mem[BC]
+	 * Print string to the stdout
+	 */
 }
 
 
 
 void GTUOS::OperationReadStr(const CPU8080 &cpu) {
-	
+	/*
+	 * TODO:
+	 * Get string from stdin
+	 * Write string to the mem[BC]
+	 */
 }
 
 
 
 void GTUOS::OperationGetRnd(const CPU8080 &cpu) {
-	
+	/*
+	 * TODO:
+	 * Produce a random integer
+	 * Place random integer to register B
+	 */	
 }
 
 
